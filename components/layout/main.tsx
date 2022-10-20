1e2147f12c3bcf3ccb6d4ec6977e0c61a3abb694
unknown <quangsang239@gmail.com>
2022-10-20 10:14:57 +0700

    config

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -2,19 +2,22 @@ import { motion } from "framer-motion";
 import ReactMapGL from "react-map-gl";
 
 import { LayoutProps } from "../../models";
-
+import { FaSearchLocation } from "react-icons/fa";
 export function MainLayout(props: LayoutProps) {
   return (
     <motion.div
-      className=" w-screen h-screen overflow-hidden"
+      className=" w-screen h-screen overflow-hidden relative"
       initial={{ scale: 0.1, opacity: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 2 }}
     >
+      <button className="w-12 h-12 absolute bg-white z-10 top-5 left-1/2 -translate-x-1/2 rounded-lg border-solid border-black border-2">
+        <FaSearchLocation className="w-8 h-8 text-rose-500 relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hover:cursor-pointer" />
+      </button>
       <ReactMapGL
         initialViewState={{
-          latitude: 21.0244246,
-          longitude: 105.7938072,
+          latitude: 10.801607983756918,
+          longitude: 106.66400649270845,
           zoom: 16,
         }}
         mapStyle="mapbox://styles/mapbox/streets-v10"
